0bd94db989bca5f21459b309f7ea4f5e48c9d781
PreetamSolulab <preetam@solulab.co>
2021-11-30 11:53:35 +0530

    Initial commit: Basic gateway setup

---
+++ router/src/index.ts
@@ -0,0 +1,21 @@
+// Load environment variables
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { ApolloServer } from 'apollo-server'
+import { ApolloGateway } from '@apollo/gateway'
+
+const gateway = new ApolloGateway()
+
+const server = new ApolloServer({
+  gateway,
+})
+
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀 Gateway ready at ${url}`)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
