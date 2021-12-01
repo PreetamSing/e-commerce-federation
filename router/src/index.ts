62b7d883bd3c73cccff459b6958b915e0d7e804d
PreetamSing <rathoresinghpreetam@gmail.com>
2021-12-02 00:00:20 +0530

    Added proxy for rest-api.

--- router/src/index.ts
+++ router/src/index.ts
@@ -8,34 +8,47 @@ if (path.extname(__filename) === '.js') {
   require('module-alias/register')
 }
 
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import express from 'express'
+import http from 'http'
+import proxy from 'express-http-proxy'
 import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway'
 
 import Context from './context'
 
-const gateway = new ApolloGateway({
-  buildService: ({ name, url }) => {
-    return new RemoteGraphQLDataSource({
-      url,
-      willSendRequest({ request, context }) {
-        // Following user will appear in the 'req.body.user' of service
-        // @ts-ignore
-        request.user = context.user
-      },
-    })
-  },
-})
+async function startApolloServer() {
+  const app = express()
 
-const server = new ApolloServer({
-  gateway,
-  context: Context,
-})
+  // Setup proxy for rest-api.
+  const REST_API_URL = process.env.REST_API_URL
+  app.use('/rest-api', proxy(REST_API_URL))
+  const httpServer = http.createServer(app)
+  const gateway = new ApolloGateway({
+    buildService: ({ name, url }) => {
+      return new RemoteGraphQLDataSource({
+        url,
+        willSendRequest({ request, context }) {
+          // Following user will appear in the 'req.body.user' of service
+          // @ts-ignore
+          request.user = context.user
+        },
+      })
+    },
+  })
+  const server = new ApolloServer({
+    gateway,
+    context: Context,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+  await server.start()
+  server.applyMiddleware({ app })
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  )
+  console.log(`🚀 Server ready at http://localhost:4000`)
+}
 
-server
-  .listen()
-  .then(({ url }) => {
-    console.log(`🚀 Gateway ready at ${url}`)
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+startApolloServer().catch((error) => {
+  console.error('Failed to start server', error)
+})
