564e9dbae9dd394a4d0fbc6eafd89ec39b958e53
PreetamSing <rathoresinghpreetam@gmail.com>
2021-12-01 22:49:53 +0530

    Added buy product query.

--- services/cart/src/models/purchase.ts
+++ services/cart/src/models/purchase.ts
@@ -5,6 +5,7 @@ const ObjectId = Schema.Types.ObjectId
 interface Purchase extends BaseModel {
   _user: typeof ObjectId
   _product: typeof ObjectId
+  paymentIntentId: string
   isAmountReceived: boolean
   amount: number
   currency?: string
@@ -14,6 +15,7 @@ const schema = new Schema<Purchase>(
   {
     _user: { type: ObjectId, required: true, ref: 'User' },
     _product: { type: ObjectId, required: true, ref: 'Product' },
+    paymentIntentId: { type: String, required: true },
     isAmountReceived: { type: Boolean, default: false },
     currency: { type: String, default: 'inr' },
 
